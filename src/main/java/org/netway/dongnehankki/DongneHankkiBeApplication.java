ed68f54d3f2b88ec5a38e172a99bf3440426de4a
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-06-14 11:07:25 +0900

    Initial setting

---
+++ src/main/java/org/netway/dongnehankki/DongneHankkiBeApplication.java
@@ -0,0 +1,13 @@
+package org.netway.dongnehankki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongneHankkiBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongneHankkiBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/netway/dongnehankki/DongneHankkiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.netway.dongnehankki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongneHankkiBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
