41204d8de04b0cc72bc8f310bc60200a39a8153c
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-07-07 19:59:14 +0900

    [feat] #12 global exception handler 추가

--- src/main/java/org/netway/dongnehankki/global/exception/ErrorCode.java
+++ src/main/java/org/netway/dongnehankki/global/exception/ErrorCode.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
-	BAD_REQUEST("400", "잘못된 요청입니다.", HttpStatus.BAD_REQUEST);
+	BAD_REQUEST("400", "잘못된 요청입니다.", HttpStatus.BAD_REQUEST),
+	INTERNAL_SERVER_ERROR("500", "서버 내부 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+	EXTERNAL_SERVICE_ERROR("500", "외부 서비스 연동 중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
 
 	private final HttpStatus status;
 	private final String code;
