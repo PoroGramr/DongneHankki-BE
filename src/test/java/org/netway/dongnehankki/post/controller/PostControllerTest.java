529108dbb299c694a52eed58423f7d0f1ba66bae
HoyiTT <pjs9177@naver.com>
2025-07-07 18:08:57 +0900

    [Test] post create 테스트 코드 init

---
+++ src/test/java/org/netway/dongnehankki/post/controller/PostControllerTest.java
@@ -0,0 +1,35 @@
+package org.netway.dongnehankki.post.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void 포스트작성() throws Exception {
+        mockMvc.perform(post("/api/post/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new PostCreateRequest()))
+            ).andDo(print())
+            .andExpect(status().isOk());
+    }
+
+
+}
