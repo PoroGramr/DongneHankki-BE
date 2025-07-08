edd6b7515197187919250997dd3d35d4dcfe355d
HoyiTT <pjs9177@naver.com>
2025-07-08 23:54:31 +0900

    [refactor]#6 jwt 토큰 모바일 환경에 맞춰 롤백

--- src/main/java/org/netway/dongnehankki/user/presentation/UserController.java
+++ src/main/java/org/netway/dongnehankki/user/presentation/UserController.java
@@ -1,14 +1,11 @@
 package org.netway.dongnehankki.user.presentation;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.netway.dongnehankki.global.common.ApiResponse;
 import org.netway.dongnehankki.user.application.UserService;
 import org.netway.dongnehankki.user.application.dto.login.LoginRequest;
 import org.netway.dongnehankki.user.application.dto.singUp.CustomerSingUpRequest;
 import org.netway.dongnehankki.user.application.dto.singUp.OwnerSingUpRequest;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,12 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
-
-    @Value("${app.cookie.secure}")
-    private boolean cookieSecure;
-
-    @Value("${jwt.access-token-expiration-minutes}")
-    private long accessTokenExpirationMinutes;
 
     @PostMapping("/customers")
     public ResponseEntity<ApiResponse<Void>> signUpCustomer(@RequestBody CustomerSingUpRequest customerSingUpRequest) {
@@ -41,18 +32,9 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<ApiResponse<Void>> login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+    public ResponseEntity<ApiResponse<String>> login(@RequestBody LoginRequest loginRequest) {
         String token = userService.login(loginRequest);
-
-        Cookie cookie = new Cookie("accessToken", token);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(cookieSecure);
-        cookie.setPath("/");
-        cookie.setMaxAge((int) (accessTokenExpirationMinutes * 60));
-
-        response.addCookie(cookie);
-
-        return ResponseEntity.ok(ApiResponse.success());
+        return ResponseEntity.ok(ApiResponse.success(token));
     }
 
 }
